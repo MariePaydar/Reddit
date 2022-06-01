c17b96d9d62e62b15cd6d7e7945ed218cbc5d63b
maryampaydar <maryampaydar812@gmail.com>
2022-06-01 15:24:23 -0700

    add color

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,5 +1,11 @@
 library my_prj.globals;
 
+import 'package:flutter/cupertino.dart';
 import 'package:reddit/data.dart';
 
-Data user = Data("","","");
+var darkModeStatus = false;
+
+Data user = Data("", "", "");
+Color LoginText = Color.fromARGB(255, 255, 255, 255);
+Color LoginBackground = Color.fromARGB(255, 0, 0, 0);
+Color LoginWidget = Color.fromARGB(255, 151, 9, 9);
