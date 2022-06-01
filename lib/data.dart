8bda1221ca25837645cd51f4af57a1f1b7a4c13f
Mahdis-Sepahvand <Mahdissep18@gmail.com>
2022-06-01 14:46:33 +0430

    change data

--- lib/data.dart
+++ lib/data.dart
@@ -1,7 +1,27 @@
+import 'package:flutter/cupertino.dart';
+
 class DataOfCommunity {
-  String name = "";
-  bool isPublic = false;
-  DataOfCommunity(this.name, this.isPublic);
+  String _name = "";
+  bool _isPublic = false;
+  bool _isDone;
+  DataOfCommunity(this._name, this._isPublic,this._isDone);
+  bool get isDone=> _isDone;
+  void setIsDone(bool currentState){
+    _isDone=currentState;
+  }
+  bool get isPublic => _isPublic;
+  set setDescription(bool value){
+    _isPublic =value;
+  }
+  String get name=> _name;
+
+  set setName(String value){
+    _name=value;
+  }
+  String get getName
+  {
+    return _name;
+  }
 }
 
 class Data {
