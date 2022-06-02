17b6c479a35fceb1295e17055d37e2ebd84a16e4
Mahdis-Sepahvand <Mahdissep18@gmail.com>
2022-06-02 22:59:11 +0430

    second change post page

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -1,22 +1,37 @@
 library my_prj.globals;
-
 import 'dart:js_util';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit/data.dart';
 import 'package:reddit/globals.dart';
 
 class Post extends StatelessWidget {
-  Post({Key? key}) : super(key: key);
+  const Post({Key? key}) : super(key: key);
 
   @override
-  StatefulWidget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          toolbarHeight: 100,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        body: PostPage(),
+      ),
+    );
+  }
+}
+
+class PostPage extends StatefulWidget {
+  const PostPage({Key? key}) : super(key: key);
+  @override
+  State<PostPage> createState() => _PostPage();
+}
+
+class _PostPage extends State<PostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
           title: Title(
             title: user.userName,
             color: Color.fromARGB(255, 113, 94, 94),
@@ -27,31 +42,11 @@ class Post extends StatelessWidget {
                   user.communitylist[0].name,
                   style: TextStyle(
                       color: Colors.white, fontWeight: FontWeight.w500, fontSize: 30),
-                )),
-            )
-          ),
-        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-        body: const PostPage(),
+                )
+
       ),
-    );
+          ),
+      ),
+      );
   }
-}
-
-class PostPage extends StatefulWidget {
-  const PostPage({Key? key}) : super(key: key);
-  @override
-  _PostPageState createState() => _PostPageState();
-}
-
-class _PostPageState extends State<PostPage> {
-  TextEditingController usernameController = TextEditingController();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
   }
-}
