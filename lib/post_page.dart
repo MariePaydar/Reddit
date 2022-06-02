bdd263beb8512f029b63945e7d170563dcce869f
Mahdis-Sepahvand <Mahdissep18@gmail.com>
2022-06-03 01:31:02 +0430

    appbar post page

--- lib/post_page.dart
+++ lib/post_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:reddit/data.dart';
 import 'package:reddit/globals.dart';
 
+import 'home_screan.dart';
+
 class Post extends StatelessWidget {
   const Post({Key? key}) : super(key: key);
 
@@ -31,22 +33,29 @@ class _PostPage extends State<PostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 100,
-          title: Title(
-            title: user.userName,
-            color: Color.fromARGB(255, 113, 94, 94),
-            child: Container(
-             alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  user.communitylist[0].name,
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w500, fontSize: 30),
-                )
-
-      ),
-          ),
-      ),
-      );
+       leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          tooltip: 'Back to setting page',
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const Feed()));
+          },
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title:RichText(
+           text:  TextSpan(children: <TextSpan>[
+                TextSpan(
+                    text: user.userName+"\n"+'salam'+'\n',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w300,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    )),
+              ])),          
+        ),
+      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+      
+     ); 
   }
   }
